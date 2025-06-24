3be56a52dc2c74d173fa3d068e5a7ed66211c846
Roberto Alfano <alfanoroberto99@gmail.com>
2025-06-24 20:28:11 +0200

    Update save_slice.py

--- save_slice.py
+++ save_slice.py
@@ -1,7 +1,7 @@
 # Create slices data in binary files from
 # PLUTO simulations
 # Author: Roberto Alfano
-# Copyright (c) 2025, [TUO NOME]
+# Copyright (c) 2025, Roberto Alfano
 # Licensed under CC BY-NC 4.0: https://creativecommons.org/licenses/by-nc/4.0/
 
 ######################## imports ########################
